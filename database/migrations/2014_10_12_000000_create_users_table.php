5b1f46aefe5599b221a75a219d3e223d3180d6cc
Kevin A <kevin.arias13m@gmail.com>
2021-07-26 18:20:05 -0500

    Migrations have been added.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('profile');
+            $table->date('birth_date');
             $table->timestamps();
+            $table->rememberToken();
         });
     }
 
